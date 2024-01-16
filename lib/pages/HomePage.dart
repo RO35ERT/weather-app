52d77bb100ba463b076d2fd9752ddc6260c6771b
RO35ERT <trebrorobertsbob@gmail.com>
2024-01-16 20:03:46 +0200

    added a fetch method

--- lib/pages/HomePage.dart
+++ lib/pages/HomePage.dart
@@ -6,6 +6,23 @@ class HomePage extends StatefulWidget {
   @override
   State<HomePage> createState() => _HomePageState();
 }
+var apiKey = "eb66d86c1805bbcfdab91296b08f9008";
+var city = "Lusaka";
+late Map<String, dynamic> weatherData;
+Future<void> fetchData() async {
+  final response = await http.get(
+    Uri.parse('https://api.openweathermap.org/data/2.5/weather?q=$city&appid=$apiKey'),
+  );
+
+  if (response.statusCode == 200) {
+    final Map<String, dynamic> data = json.decode(response.body);
+    setState(() {
+      weatherData = data;
+    });
+  } else {
+    throw Exception('Failed to load weather data');
+  }
+}
 
 class _HomePageState extends State<HomePage> {
   @override
