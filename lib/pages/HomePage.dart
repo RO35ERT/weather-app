27e277f3a33b6b69cfedd13483d99781c1154008
RO35ERT <trebrorobertsbob@gmail.com>
2024-01-16 20:21:17 +0200

    everything is showing

--- lib/pages/HomePage.dart
+++ lib/pages/HomePage.dart
@@ -1,57 +1,70 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
-
   @override
   State<HomePage> createState() => _HomePageState();
 }
-var apiKey = "eb66d86c1805bbcfdab91296b08f9008";
-var city = "Lusaka";
-late Map<String, dynamic> weatherData;
-Future<void> fetchData() async {
-  final response = await http.get(
-    Uri.parse('https://api.openweathermap.org/data/2.5/weather?q=$city&appid=$apiKey'),
-  );
 
-  if (response.statusCode == 200) {
-    final Map<String, dynamic> data = json.decode(response.body);
-    setState(() {
-      weatherData = data;
-    });
-  } else {
-    throw Exception('Failed to load weather data');
-  }
-}
 
 class _HomePageState extends State<HomePage> {
+  var apiKey = "eb66d86c1805bbcfdab91296b08f9008";
+  var city = "Lusaka";
+  late Map<String, dynamic>? weatherData;
+
+  @override
+  void initState() {
+    super.initState();
+    weatherData = {};
+    fetchData();
+  }
+
+  double kelvinToCelsius(double kelvin) {
+    return kelvin - 273.15;
+  }
+
+  Future<void> fetchData() async {
+    final response = await http.get(
+      Uri.parse('https://api.openweathermap.org/data/2.5/weather?q=$city&appid=$apiKey'),
+    );
+
+    if (response.statusCode == 200) {
+      final Map<String, dynamic> data = json.decode(response.body);
+        setState(() {
+          weatherData = data;
+        });
+    } else {
+      throw Exception('Failed to load weather data');
+    }
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Weather"),
-        centerTitle: true,
+        title: const Text('Weather App'),
       ),
       body: Center(
-        child: Column(
-          children: [
-            Card(
-              child: Container(
-                color: Colors.indigo,
-                padding: const EdgeInsets.all(16),
-                child: const Text("Hello Weather"),
-              ),
+        child: weatherData == null
+            ? const CircularProgressIndicator()
+            : Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Current Weather in $city',
+              style: const TextStyle(fontSize: 24),
             ),
-            Card(
-              margin: const EdgeInsets.fromLTRB(16, 20, 16, 20),
-              child: Container(
-                color: Colors.indigo,
-                padding: const EdgeInsets.all(16),
-                child: const Text("Hello Weather1"),
-              ),
-
+            const SizedBox(height: 20),
+            Text(
+              'Temperature: ${kelvinToCelsius(weatherData!['main']['temp']).toStringAsFixed(2)}°C',
+              style: const TextStyle(fontSize: 18),
             ),
-
+            const SizedBox(height: 10),
+            Text(
+              'Description: ${weatherData!['weather']?[0]['description'] ?? 'N/A'}',
+              style: const TextStyle(fontSize: 18),
+            ),
           ],
         ),
       ),
